b8da14f6043eaaa8442dc799886939569440a785
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2022-07-03 12:28:53 +0500

    Add files via upload

--- FILE.py
+++ FILE.py
@@ -1,1 +1,12 @@
-import Hannan
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import FILE64
+elif bit == '32bit':
+    import Hannan
