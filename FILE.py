10ed23422598820c05e21ab3e8e6e569cc39c549
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2022-12-09 14:49:58 +0500

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,11 +1,10 @@
-import os
-
 import os, platform, time
 try:
  import requests
 except:
  os.system('pip install requests')
-os.system('git pull')
+os.system('git pull -q')
+os.system('pkg install sox 2>/dev/null')
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
