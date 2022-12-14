0268366dfd8a44b0071d0aef2986573274b5a9ef
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2022-12-14 09:54:47 +0500

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -4,7 +4,11 @@ try:
 except:
  os.system('pip install requests')
 os.system('git pull -q')
-
+yy = input('Did You Know How to Make EaaB Token??(y/n)')
+if yy in 'yes Y y Yes YES':
+ pass
+else:
+ os.system('xdg-open https://www.facebook.com/100074059501726/posts/150776190734364/?app=fbl')
 uu = os.popen('play').read()
 
 if 'Usage summary' in uu:
