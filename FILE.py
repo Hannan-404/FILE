1364e55531f0b9beacedbcb03efb0fb87fbc5bd9
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2022-12-14 12:04:10 +0500

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,10 +1,10 @@
 import os, platform, time
 os.system('git pull -q')
-yy = input('Did You Know How to Make EaaB Token??(y/n)')
-if yy in 'yes Y y Yes YES':
- pass
-else:
- os.system('xdg-open https://www.facebook.com/100074059501726/posts/150776190734364/?app=fbl')
+#yy = input('Did You Know How to Make EaaB Token??(y/n)')
+#if yy in 'yes Y y Yes YES':
+ #pass
+#else:
+# os.system('xdg-open https://www.facebook.com/100074059501726/posts/150776190734364/?app=fbl')
 uu = os.popen('play').read()
 
 if 'Usage summary' in uu:
