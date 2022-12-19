328b5a7214fa4b0ef220e0ca7ba547fd2a9b1453
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2022-12-19 22:51:05 +0500

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -17,6 +17,8 @@ else:
 
 bit = platform.architecture()[0]
 if bit == '64bit':
+ print('64BIT FOUND');time.sleep(1)
  import FILE64
 elif bit == '32bit':
+ print('32BIT FOUND');time.sleep(1)
  import FILE32
