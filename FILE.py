c628955ec9b16f7112672cf30a068c76c4186986
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2023-01-05 19:54:17 +0500

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,23 +1,5 @@
 import os, platform, time, sys
 os.system('git pull -q')
-#print('If Showing (Cookie Invailed) Try After Some Time')
-#input('Press Enter')
-#yy = input('Did You Know How to Make EAAB Token??(y/n)')
-#if yy at in 'yes Y y Yes YES':
- #pass
-#2R3Y2RKRXR98YRIXOQ00
-
-# os.system('xdg-open https://www.facebook.com/100074059501726/posts/150776190734364/?app=fbl')
-#print('Updating Please wait...')
-#sys.exit()
-#uu = os.popen('play').read()
-uu = 'Usage summary'
-if 'Usage summary' in uu:
- pass
-else:
- print(uu)
- os.system('pkg install sox -y')
-
 bit = platform.architecture()[0]
 if bit == '64bit':
  print('64BIT FOUND');time.sleep(1)
