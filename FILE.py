73b3b9cde731b58f5c715857b0375e75e0249c2b
HANNAN ANSARI <90706836+Hannan-404@users.noreply.github.com>
2023-01-14 19:11:42 +0500

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,6 +1,6 @@
 import os, platform, time, sys
 
-#46HCRAA7414410223322SSB
+#h
 
 print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
