dc46da9ba259fee0b28cafee86f9c6fa580425bd
Hannan AnSari <90706836+Hannan-404@users.noreply.github.com>
2024-10-10 17:25:40 +0500

    Update FILE.py

--- FILE.py
+++ FILE.py
@@ -1,15 +1,2 @@
-import os, platform, time, sys
-os.system('xdg-open https://chat.whatsapp.com/L9x5eIlqlX1LZjipz265rV')
-try:
- import requests
-except:os.system("pip uninstall requests -y;pip install requests")
-
-print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
-os.system('git pull --quiet 2>/dev/null')
-bit = platform.architecture()[0]
-if bit == '64bit':
- print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97m64Bit Found')
- import FILE64
-elif bit == '32bit':
- print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97m32Bit Found')
- import FILE32
+print("[+] check github for new commands! ")
+print("https://github.com/Hannan-404/FILE")
